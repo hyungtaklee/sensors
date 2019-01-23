6c30214f47adafd7e769d626dc6b8c68b0816399
HT <21leeht@gmail.com>
2019-01-23 16:34:39 +0900

    fix parentheses error of sock()

--- ipc/server.c
+++ ipc/server.c
@@ -20,12 +20,13 @@ struct sensor_data { /* 16 bytes */
 int main(int argc, char **argv)
 {
 	int listenfd, connfd;
-	struct sockaddr_in servaddr, clientaddr;
-
-	int clientlen;
+	struct sockaddr_in servaddr, cliaddr;
+	int err;
+	int clilen;
 	char tempip[20];
-
 	struct sensor_data recv_data;
+
+	int port;
 
 	if (argc != 2) {
 		printf("usage: %s [server_port]\n", argv[0]);
@@ -33,14 +34,15 @@ int main(int argc, char **argv)
 	}
 	
 	/* check port number */
-	if (atoi(argv[1]) < 0 || atoi(argv[1]) > 65535) {
+	port = atoi(argv[1]);
+	if (port < 0 || port > 65535) {
 		printf("fatal: invalid port number\n");
 		return 0;
 	}
 
 	/* privilege warn */
 	if (getuid() <= 0 || getuid() != geteuid()) {
-		if (atoi(argv[1]) > 1023) {
+		if (port > 1023) {
 			printf("warning: running server with root privilege\n");
 		} else {
 			printf("warning: running server program "
@@ -49,23 +51,21 @@ int main(int argc, char **argv)
 	}
 
 	/* open socket */
-	if (listenfd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		fprintf(stderr, "fatal: socket error\n");
 		return -1;
 	}
-
 	/* intialize servaddr structure */
 	memset(&servaddr, 0, sizeof(servaddr));
 	
 	/* set value of servaddr */
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(atoi(argv[1]));
-	
+	servaddr.sin_port = htons(port);
 	/* bind socket and file descriptor */
-	if (bind(listenfd, (struct sockaddr *) &servaddr,
-		  sizeof(servaddr)) < 0) {
-		printf("fatal: bind error\n");
+	err = bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	if (err < 0) {
+		printf("fatal: bind error, bind() returns value %d\n", err);
 		return 0;
 	}
 	
@@ -75,17 +75,17 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	clientlen = sizeof(clientaddr);
+	clilen = sizeof(cliaddr);
 
 	for ( ; ; ) {
 		connfd = accept(listenfd, 
-			(struct sockaddr *) &clientaddr, &clientlen);
+			(struct sockaddr *) &cliaddr, &clilen);
 		if (connfd < 0) {
 			printf("fatal: accept error\n");
 			return 0;
 		}
 		/* show connection information */
-		inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr,
+		inet_ntop(AF_INET, &cliaddr.sin_addr.s_addr,
 				tempip, sizeof(tempip));
 		printf("server: connected with %s\n", tempip);
 	
