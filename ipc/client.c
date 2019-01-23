b2afabf79b7959f0a88831aacf2bf9df6dac554a
HT <21leeht@gmail.com>
2019-01-23 15:26:02 +0900

    add ipc client and server

---
+++ ipc/client.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+
+#define MAXLINE 128
+
+struct sensor_data {
+	char sensor_name[64];
+	double value;
+};
+
+int main(int argc, char **argv)
+{
+	/* variables for network communtiation */
+	int sockfd, n;
+	char recvline[MAXLINE + 1];
+	struct sockaddr_in servaddr;
+
+	/* variables for sensor */
+	long dist, duration;
+	time_t start_time, end_time, time_diff;
+	double time_diff_sec;	
+	double distance;
+
+	struct sensor_data data;
+
+	/* check argument */
+	if (argc != 2) {
+		printf("usage: %s [server_port]\n", argv[0]);
+		return 0;
+	}
+	
+	/* check port number */
+	if (atoi(argv[1]) < 0 && atoi(argv[0]) > 65535) {
+		printf("error: invalid port number\n");
+		return 0;
+	}
+	
+	/* setup wiringPi */
+	if (wiringPiSetup() == -1) {
+		printf("error: wiringPi setup failed\n");
+		return -1;
+	}
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("error: socket error\n");
+		return -1;
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(atoi(argv[1]));
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+		printf("error: inet_pton error for %s\n", argv[1]);
+		return -1;
+	}
+
+	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))< 0){
+		printf("error: connect error\n");
+		return -1;
+	}
+
+	/* initialize sensor */
+	pinMode(TRIG_PIN, OUTPUT);
+	pinMode(ECHO_PIN, INPUT);
+
+	digitalWrite(TRIG_PIN, 0);
+	delay(1000);
+
+	printf("Start!\n");
+
+	for (;;) {
+		digitalWrite(TRIG_PIN, 0);
+		delay(500); // write LOW for 500ms
+
+		digitalWrite(TRIG_PIN, 1);
+		delayMicroseconds(10);
+		
+		digitalWrite(TRIG_PIN, 0);
+		
+		while (digitalRead(ECHO_PIN) == 0)
+			start_time = clock();
+
+		while(digitalRead(ECHO_PIN) == 1) {
+			end_time = clock();
+		}
+		time_diff_sec = (double)(end_time - start_time) 
+					/ CLOCKS_PER_SEC;
+		
+		data.value = time_diff_sec * 17000;
+		printf("distance: %.3lf\n", data_value);
+
+		write(sockfd, data, sizeof(data));
+
+		delay(300);
+	}
+
+	return 0;
+}	
---
+++ ipc/server.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <unistd.h>
+
+#define LISTENQ 1024
+#define BUFFLEN 8096
+
+struct sensor_data { /* 16 bytes */
+	char sensor_name[64]; /* 8 bytes */
+	double value; /* 8 bytes */
+};
+
+int main(int argc, char **argv)
+{
+	int listenfd, connfd;
+	struct sockaddr_in servaddr, clientaddr;
+	char buff[BUFFLEN];
+	int err;
+	time_t ticks;
+
+	int clientlen;
+	char tempip[20];
+
+	struct sensor_data recv_data;
+
+	if (argc != 2) {
+		printf("usage: %s [server_port]\n", argv[0]);
+		return 0;
+	}
+	
+	/* check port number */
+	if (atoi(argv[1]) < 0 || atoi(argv[1]) > 65535) {
+		printf("fatal: invalid port number\n");
+		return 0;
+	}
+
+	/* privilege warn */
+	if (getuid() <= 0 || getuid() != geteuid()) {
+		if (atoi(argv[1]) > 1023) {
+			printf("warning: running server with root privilege\n");
+		} else {
+			printf("warning: running server program "
+			"with port number smaller than 1024\n");
+		}
+	}
+
+	/* open socket */
+	if (listenfd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+		fprintf(stderr, "fatal: socket error\n");
+		return -1;
+	}
+
+	/* intialize servaddr structure */
+	memset(&servaddr, 0, sizeof(servaddr));
+	
+	/* set value of servaddr */
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi(argv[1]));
+	
+	/* bind socket and file descriptor */
+	if (bind(listenfd, (struct sockaddr *) &servaddr,
+		  sizeof(servaddr)) < 0) {
+		printf("fatal: bind error\n");
+		return 0;
+	}
+	
+	/* listen */
+	if ((listen(listenfd, LISTENQ)) < 0) {
+		printf("fatal: listen error\n");
+		return 0;
+	}
+
+	clientlen = sizeof(clientaddr);
+
+	for ( ; ; ) {
+		connfd = accept(listenfd, 
+			(struct sockaddr *) &clientaddr, &clientlen);
+		if (connfd < 0) {
+			printf("fatal: accept error\n");
+			return 0;
+		}
+		/* show connection information */
+		inet_ntop(AF_INET, &clientaddr.sin_addr.s_addr,
+				tempip, sizeof(tempip));
+		printf("server: connected with %s\n", tempip);
+	
+		/* read sensor data from client */
+		read(connfd, &recv_data, sizeof(recv_data));
+		printf("received: device[%s], value[%lf]\n",
+				recv_data.sensor_name, recv_data.value);
+
+		close(connfd);
+	}
+	close(listenfd);
+	return 0;
+}
